e5fe812eef78b58c20c6becbd92b67bbada00785
kk488 <78923924+kk488@users.noreply.github.com>
2021-05-21 17:38:48 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -14,3 +14,7 @@ The smallest number is 3 and the largest number is 9
 """
 
 myList = [ 3,6,5,4,6,7,8,6,5,9,4,5 ]
+sortedlist=sorted(myList)
+smallest=min(sortedlist)
+largest=max(sortedlist)
+print("the smallest number is",smallest,"and the largest number is",largest)
